ea2c3c6945c0b71e9160547c9af0007564ea3c06
204andreaaa <204andreaaa@gmail.com>
2026-02-27 10:22:49 +0700

    update faker

--- database/seeders/QuoteRequestSeeder.php
+++ database/seeders/QuoteRequestSeeder.php
@@ -5,13 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\GensetSpec;
 use App\Models\GensetInquiry;
-
+use Faker\Factory as Faker; // 👈 TAMBAHIN INI
 
 class QuoteRequestSeeder extends Seeder
 {
     public function run(): void
     {
-        // Ambil semua spec yang sudah ada
+        $faker = Faker::create(); // 👈 TAMBAHIN INI
+
         $specs = GensetSpec::all();
 
         if ($specs->count() === 0) {
@@ -19,17 +20,16 @@ class QuoteRequestSeeder extends Seeder
             return;
         }
 
-        // Generate 20 dummy quote requests
         for ($i = 1; $i <= 2; $i++) {
 
             $spec = $specs->random();
 
             GensetInquiry::create([
-                'name' => fake()->name(),
-                'email' => fake()->safeEmail(),
-                'phone' => fake()->phoneNumber(),
-                'address' => fake()->address(),
-                'note' => fake()->sentence(),
+                'name' => $faker->name(),
+                'email' => $faker->safeEmail(),
+                'phone' => $faker->phoneNumber(),
+                'address' => $faker->address(),
+                'note' => $faker->sentence(),
                 'brand_id' => $spec->brand_id,
                 'genset_spec_id' => $spec->id,
             ]);
